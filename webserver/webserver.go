c574ebe96687522fcbeba0a3298611abb7409c94
Jim Shaw <jimxshaw@gmail.com>
2017-10-28 17:15:16 -0500

    webserver: render basic html page.

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -1,13 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"html"
+	"bytes"
+	"html/template"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+// PageData - This is a basic struct to hold page
+// data variables.
+type PageData struct {
+	Title string
+	Body  string
+}
+
+// Define a basic html template.
+const html = `
+			<html>
+			<head><title>{{.Title}}</title></head>
+			<body>
+			<h1>{{.Title}}</h1>
+			{{.Body}}
+			</body>
+			</html>
+			`
 
 func main() {
 	// Create a router.
@@ -21,7 +39,45 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", rt))
 }
 
-// Index This method is the "index" route handler.
+// Index - This method is the "index" route handler.
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world from %q", html.EscapeString(r.URL.Path))
+	// Fill out page data for index.
+	pd := PageData{
+		Title: "Index Page",
+		Body:  "This is the body of the page.",
+	}
+
+	// Render a template with our page data.
+	tmpl, err := render(pd)
+
+	// If we get an error, write it out and exit.
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	// If all went well, write out the template.
+	w.Write([]byte(tmpl))
+}
+
+func render(pd PageData) (string, error) {
+	// Parse the template.
+	tmpl, err := template.New("index").Parse(html)
+
+	if err != nil {
+		return "", err
+	}
+
+	// We need somewhere to write out the executed template.
+	var out bytes.Buffer
+
+	// Render the template with the data we passed in.
+	if err := tmpl.Execute(&out, pd); err != nil {
+		// If we couldn't render, return an error.
+		return "", err
+	}
+
+	// Return the template.
+	return out.String(), nil
 }
