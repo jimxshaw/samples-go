dcca25e236cc7bb1b899f66a5e36073da7976a4b
Jim Shaw <jimxshaw@gmail.com>
2017-10-28 15:46:50 -0500

    Add waitgroup.

--- cli/cli.go
+++ cli/cli.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"strings"
+	"sync"
 	"time"
 )
 
@@ -14,27 +16,49 @@ func main() {
 	// flag.Args contains all non-flag arguments.
 	sites := flag.Args()
 
+	// Args might be a single string with spaces to delimit
+	//sites, break them apart.
+	if len(sites) == 1 {
+		sites = strings.Split(sites[0], " ")
+	}
+
+	// Use a waitgroup to make sure all go routines finish.
+	var wg sync.WaitGroup
+
 	// Lets keep a reference to when we started.
 	start := time.Now()
 
-	processSites(sites)
+	// Set the value for the waitgroup.
+	wg.Add(len(sites))
+	processSites(sites, &wg)
+
+	// Block until all routines finish.
+	wg.Wait()
 
 	fmt.Println("Entire process took %s\n", time.Since(start))
 }
 
-func processSites(sites []string) {
+func processSites(sites []string, wg *sync.WaitGroup) {
 	for i, site := range sites {
-		// Start a timer for this request.
-		begin := time.Now()
+		// Launch each retrieveal in a go routine.
+		// This makes each request concurrent.
+		go func(index int, site string) {
+			// A waitgroup is essentially a counter and
+			// calling done is a way to decrement a counter.
+			defer wg.Done()
 
-		// Retrieve the site
-		resp, err := http.Get(site)
+			// Start a timer for this request.
+			begin := time.Now()
 
-		if err != nil {
-			fmt.Println(site, err)
-			continue
-		}
+			// Retrieve the site
+			resp, err := http.Get(site)
 
-		fmt.Printf("%d) Site %q took %s to retrieve with status code of %d.\n", i, site, time.Since(begin), resp.StatusCode)
+			if err != nil {
+				fmt.Println(site, err)
+				return
+			}
+
+			fmt.Printf("%d) Site %q took %s to retrieve with status code of %d.\n", i, site, time.Since(begin), resp.StatusCode)
+		}(i, site)
 	}
 }
