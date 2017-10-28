4a0c352c28e86e4d239d246f9c587730cfe53e4a
Jim Shaw <jimxshaw@gmail.com>
2017-10-28 15:30:04 -0500

    Add processSites function.

--- cli/cli.go
+++ cli/cli.go
@@ -3,11 +3,38 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
+	"time"
 )
 
 func main() {
+	// Parse all arguments.
 	flag.Parse()
 
 	// flag.Args contains all non-flag arguments.
-	fmt.Println(flag.Args())
+	sites := flag.Args()
+
+	// Lets keep a reference to when we started.
+	start := time.Now()
+
+	processSites(sites)
+
+	fmt.Println("Entire process took %s\n", time.Since(start))
+}
+
+func processSites(sites []string) {
+	for i, site := range sites {
+		// Start a timer for this request.
+		begin := time.Now()
+
+		// Retrieve the site
+		resp, err := http.Get(site)
+
+		if err != nil {
+			fmt.Println(site, err)
+			continue
+		}
+
+		fmt.Printf("%d) Site %q took %s to retrieve with status code of %d.\n", i, site, time.Since(begin), resp.StatusCode)
+	}
 }
