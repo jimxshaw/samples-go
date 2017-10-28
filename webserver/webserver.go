bde65fa5bc454789a43f493fe0030a7ae279705d
Jim Shaw <jimxshaw@gmail.com>
2017-10-28 16:48:25 -0500

    webserver: incorporate gorilla/mux router.

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -5,13 +5,23 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world from %q", html.EscapeString(r.URL.Path))
-	})
+	// Create a router.
+	rt := mux.NewRouter().StrictSlash(true)
 
+	// Add the "index" or root path.
+	rt.HandleFunc("/", Index)
+
+	// Fire up the server.
 	log.Println("Starting server on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", rt))
+}
+
+// Index This method is the "index" route handler.
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world from %q", html.EscapeString(r.URL.Path))
 }
