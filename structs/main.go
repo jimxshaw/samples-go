34499ce994f2d2936c33d0e7b5bce15695980a0d
Jim Shaw <jimxshaw@gmail.com>
2017-11-23 15:01:19 -0600

    structs: add comments.

--- structs/main.go
+++ structs/main.go
@@ -25,12 +25,23 @@ func main() {
 		},
 	}
 
+	// Turn memory address into value with *address.
+	// Turn value into memory address with &value.
+
+	// The ampersand & is an operator that says give
+	// the memory address of the value to which this
+	// variable is pointing.
 	jimPointer := &jim
-	jimPointer.updateName("Tony")
+	jimPointer.updateName("James")
 	jimPointer.print()
+
 }
 
+// An asterisk * in front of a type means a type description.
+// It says we're working with a pointer to a person type.
 func (pointerToPerson *person) updateName(newFirstName string) {
+	// An asterisk * in front of an actual pointer means we
+	// want to manipulate the value that pointer is referencing.
 	(*pointerToPerson).firstName = newFirstName
 }
 
