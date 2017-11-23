8bb20d179cd8f7c1ea2c68139481311f40b5deb6
Jim Shaw <jimxshaw@gmail.com>
2017-11-23 15:07:56 -0600

    structs: add more comments on pointers.

--- structs/main.go
+++ structs/main.go
@@ -31,9 +31,16 @@ func main() {
 	// The ampersand & is an operator that says give
 	// the memory address of the value to which this
 	// variable is pointing.
-	jimPointer := &jim
-	jimPointer.updateName("James")
-	jimPointer.print()
+	//jimPointer := &jim
+	//jimPointer.updateName("James")
+
+	// This is a shortcut that does the exact same
+	// thing as the above two lines.
+	// In Go, if we define a receiver function with a pointer
+	// to a type, Go will allow either that pointer
+	// type or that type itself to call the function.
+	jim.updateName("Jimmy")
+	jim.print()
 
 }
 
