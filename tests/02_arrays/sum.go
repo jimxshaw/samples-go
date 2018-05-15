b9c21d99cbeb288dd140aa52237ce06d8c16bc13
Jim Shaw <jimxshaw@gmail.com>
2018-05-14 20:32:16 -0500

    Refactor sum function to take in a slice.

--- tests/02_arrays/sum.go
+++ tests/02_arrays/sum.go
@@ -1,7 +1,7 @@
 package main
 
 // Sum adds a collection of numbers
-func Sum(numbers [5]int) (sum int) {
+func Sum(numbers []int) (sum int) {
 	for _, num := range numbers {
 		sum += num
 	}
--- tests/02_arrays/sum_test.go
+++ tests/02_arrays/sum_test.go
@@ -3,12 +3,21 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{2, 4, 6, 8, 10}
+	assertCorrectMessage := func(t *testing.T, got, want int, numbers []int) {
+		t.Helper()
 
-	got := Sum(numbers)
-	want := 30
-
-	if want != got {
-		t.Errorf("got %d want %d give, %v", got, want, numbers)
+		if want != got {
+			t.Errorf("got %d want %d give, %v", got, want, numbers)
+		}
 	}
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{2, 4, 6}
+
+		got := Sum(numbers)
+		want := 12
+
+		assertCorrectMessage(t, got, want, numbers)
+	})
+
 }
