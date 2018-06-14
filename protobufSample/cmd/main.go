10f2f95c34f0fc42bc6a976fd2a31d9a555fc47b
Jim Shaw <jimxshaw@gmail.com>
2018-06-14 11:47:58 -0500

    Refactor list and add functions.

--- protobufSample/cmd/main.go
+++ protobufSample/cmd/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"flag"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -14,14 +15,12 @@ import (
 
 func main() {
 	flag.Parse()
-
 	if flag.NArg() < 1 {
 		fmt.Fprintln(os.Stderr, "missing subcommand: list or add")
 		os.Exit(1)
 	}
 
 	var err error
-
 	switch cmd := flag.Arg(0); cmd {
 	case "list":
 		err = list()
@@ -30,41 +29,20 @@ func main() {
 	default:
 		err = fmt.Errorf("unknown subcommand %s", cmd)
 	}
-
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-
 }
 
-const dbPath = "mydb.pb"
+type length int64
 
-func list() error {
-	b, err := ioutil.ReadFile(dbPath)
-	if err != nil {
-		return fmt.Errorf("Could not read file %s: %v", dbPath, err)
-	}
+const (
+	sizeOfLength = 8
+	dbPath       = "mydb.pb"
+)
 
-	for {
-		var task todo.Task
-
-		if err := proto.Unmarshal(b, &task); err == io.EOF {
-			return nil
-		} else if err != nil {
-			return fmt.Errorf("Could not read task: %v", err)
-		}
-
-		if task.Done {
-			fmt.Printf("[O]")
-		} else {
-			fmt.Printf("[X]")
-		}
-
-		fmt.Printf(" %s\n", task.Text)
-	}
-
-}
+var endianness = binary.LittleEndian
 
 func add(text string) error {
 	task := &todo.Task{
@@ -74,22 +52,58 @@ func add(text string) error {
 
 	b, err := proto.Marshal(task)
 	if err != nil {
-		return fmt.Errorf("Could not encode task: %v", err)
+		return fmt.Errorf("could not encode task: %v", err)
 	}
 
 	f, err := os.OpenFile(dbPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		return fmt.Errorf("Could not open %s: %v", dbPath, err)
+		return fmt.Errorf("could not open %s: %v", dbPath, err)
 	}
 
+	if err := binary.Write(f, endianness, length(len(b))); err != nil {
+		return fmt.Errorf("could not encode length of message: %v", err)
+	}
 	_, err = f.Write(b)
 	if err != nil {
-		return fmt.Errorf("Could not write task to file: %v", err)
+		return fmt.Errorf("could not write task to file: %v", err)
 	}
 
 	if err := f.Close(); err != nil {
-		return fmt.Errorf("Could not close file %s: %v", dbPath, err)
+		return fmt.Errorf("could not close file %s: %v", dbPath, err)
+	}
+	return nil
+}
+
+func list() error {
+	b, err := ioutil.ReadFile(dbPath)
+	if err != nil {
+		return fmt.Errorf("could not read %s: %v", dbPath, err)
 	}
 
-	return nil
+	for {
+		if len(b) == 0 {
+			return nil
+		} else if len(b) < sizeOfLength {
+			return fmt.Errorf("remaining odd %d bytes, what to do?", len(b))
+		}
+
+		var l length
+		if err := binary.Read(bytes.NewReader(b[:sizeOfLength]), endianness, &l); err != nil {
+			return fmt.Errorf("could not decode message length: %v", err)
+		}
+		b = b[sizeOfLength:]
+
+		var task todo.Task
+		if err := proto.Unmarshal(b[:l], &task); err != nil {
+			return fmt.Errorf("could not read task: %v", err)
+		}
+		b = b[l:]
+
+		if task.Done {
+			fmt.Printf("👍")
+		} else {
+			fmt.Printf("😱")
+		}
+		fmt.Printf(" %s\n", task.Text)
+	}
 }
