85bf3540f7fe4acca9139976f2dfda626b42c4ae
Jim Shaw <jimxshaw@gmail.com>
2019-02-12 17:06:59 -0600

    Add pointers test files.

---
+++ tests/04_pointers/wallet.go
@@ -0,0 +1,14 @@
+package main
+
+// Wallet struct
+type Wallet struct{}
+
+// Deposit will deposit money
+func (w Wallet) Deposit(amount int) {
+
+}
+
+// Balance will show the balance
+func (w Wallet) Balance() int {
+	return 0
+}
