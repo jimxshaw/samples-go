bfc0fda868f4f6ebd9eeb1751325a048d7265c18
Jim Shaw <jimxshaw@gmail.com>
2019-02-18 15:35:21 -0600

    Write Withdraw method. Add Withdraw test.

--- tests/04_pointers/wallet.go
+++ tests/04_pointers/wallet.go
@@ -17,10 +17,15 @@ type Wallet struct {
 	balance Bitcoin
 }
 
-// Deposit will deposit money
+// Deposit will put in money
 func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Println("address of balance in Deposit is", &w.balance)
 	w.balance += amount
+}
+
+// Withdraw will take out money
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
 
 // Balance will show the balance
--- tests/04_pointers/wallet_test.go
+++ tests/04_pointers/wallet_test.go
@@ -6,17 +6,35 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	got := wallet.Balance()
+		wallet.Deposit(Bitcoin(10))
 
-	fmt.Println("address of balance in test is", &wallet.balance)
+		got := wallet.Balance()
 
-	want := Bitcoin(10)
+		fmt.Println("address of balance in test is", &wallet.balance)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(10)
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
 }
