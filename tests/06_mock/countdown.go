de7cf893fc118e164865d045fca4158d099b8900
Jim Shaw <jimxshaw@gmail.com>
2019-02-24 17:02:21 -0600

    Implement countdown method.

--- tests/06_mock/countdown.go
+++ tests/06_mock/countdown.go
@@ -8,7 +8,10 @@ import (
 
 // Countdown counts down
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, "Go!")
 }
 
 func main() {
