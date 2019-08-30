74a01c8f2c9390a4ea80bf1458c92b3e4645855f
Jim Shaw <jimxshaw@gmail.com>
2019-08-29 21:44:43 -0500

    Add more lines for check test coverage.

--- testing/01/math.go
+++ testing/01/math.go
@@ -3,6 +3,12 @@ package pack
 // Add sums two ints together.
 func Add(nums ...int) int {
 	var result int
+
+	if len(nums) == 0 {
+		println("No arguments provided")
+		return 0
+	}
+
 	for _, i := range nums {
 		result += i
 	}
