3ddfe0bc9c5f2cee4970c265ae6be86ed0f45c31
Jim Shaw <jimxshaw@gmail.com>
2021-02-17 15:42:15 -0600

    Update exercise 2a.

--- fem-go/02_introduction/code/exercise_2a.go
+++ fem-go/02_introduction/code/exercise_2a.go
@@ -5,5 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 
-	fmt.Printf("Hi! My name is James. I have lived in Dallas for many years. They say the weather is amazing, which is false.")
+	fmt.Printf("Hi! My name is %s. I have lived in Dallas for %d years. They say the weather is amazing, which is %t.", "James", 20, false)
 }
