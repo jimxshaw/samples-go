c9af4ffa32b6b66ba1c0b421b6e6952808b9ae56
Jim Shaw <jimxshaw@gmail.com>
2021-02-17 15:39:03 -0600

    Exercise 2a.

---
+++ fem-go/02_introduction/code/exercise_2a.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+
+	fmt.Printf("Hi! My name is James. I have lived in Dallas for many years. They say the weather is amazing, which is false.")
+}
