61514aad3a6bc88a7c605286a96a5eadbf4404ed
Jim Shaw <jimxshaw@gmail.com>
2022-08-04 22:00:41 -0500

    containersFromScratch: add boilerplate main func.

--- containersFromScratch/main.go
+++ containersFromScratch/main.go
@@ -1,8 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 // Build a container from scratch.
 // docker					run image <cmd> <params>
 // go run main.go run				<cmd> <params>
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		panic("Bad Command!")
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+}
 
 func must(err error) {
 	if err != nil {
