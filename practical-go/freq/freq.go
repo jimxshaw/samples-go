7fb1f3b1e1c981b9d0a43c6303fd230f0d0a1de5
Jim Shaw <jimxshaw@gmail.com>
2023-08-17 11:09:47 -0500

    Update freq example.

--- practical-go/freq/freq.go
+++ practical-go/freq/freq.go
@@ -19,9 +19,58 @@ func main() {
 	}
 	defer file.Close()
 	wordFrequency(file)
+
+	mapDemo()
+
+	// `s` is a "raw" string where \ is just a \
+	// rather than escape character like \n.
+	path := `C:\to\new\folder\report.csv`
+	fmt.Println(path)
+}
+
+func mapDemo() {
+	var stocks map[string]float64 // symbol -> count
+	sym := "GOOG"
+	price := stocks[sym]
+	fmt.Printf("%s -> $%.2f\n", sym, price)
+
+	if price, ok := stocks[sym]; ok {
+		fmt.Printf("%s -> $%.2f\n", sym, price)
+	} else {
+		fmt.Printf("%s not found\n", sym)
+	}
+
+	// stocks[sym] = 483.27 // cannot do this before initializing the map first
+	stocks = make(map[string]float64)
+	stocks[sym] = 483.27
+	stocks["AAPL"] = 363.85
+
+	if price, ok := stocks[sym]; ok {
+		fmt.Printf("%s -> $%.2f\n", sym, price)
+	} else {
+		fmt.Printf("%s not found\n", sym)
+	}
+
+	for k := range stocks { // keys
+		fmt.Println(k)
+	}
+
+	for k, v := range stocks { // keys & values
+		fmt.Println(k, " -> ", v)
+	}
+
+	for _, v := range stocks { // values
+		fmt.Println(v)
+	}
+
+	delete(stocks, "AAPL")
+	fmt.Println(stocks)
+	// fmt.Printf("%p\n", &stocks) // must use Printf for actual memory address
+	delete(stocks, "AAPL") // no panic
 }
 
 // Variables will execute before main.
+// "What's your name?" -> [What s your name]
 var wordRegex = regexp.MustCompile(`[a-zA-Z]+`)
 
 // The init function will also execute before main.
@@ -33,7 +82,11 @@ func wordFrequency(r io.Reader) (map[string]int, error) {
 
 	for s.Scan() {
 		lnum++
-		s.Text() // current line
+		words := wordRegex.FindAllString(s.Text(), -1) // current line
+		if len(words) != 0 {
+			fmt.Println(words)
+			break
+		}
 	}
 
 	if err := s.Err(); err != nil {
