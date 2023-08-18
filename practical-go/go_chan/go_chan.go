1ce8af4cc77b64f9a6b8d5e118340c86eb82ab65
Jim Shaw <jimxshaw@gmail.com>
2023-08-18 16:14:02 -0500

    Update go chan example.

--- practical-go/go_chan/go_chan.go
+++ practical-go/go_chan/go_chan.go
@@ -17,15 +17,23 @@ func main() {
 		// }()
 
 		// Fix 1: use a parameter
-		// go func(n int) {
-		// 	fmt.Println(n)
-		// }(i)
+		go func(n int) {
+			fmt.Println(n)
+		}(i)
 
 		// Fix 2: use a loop body variable
-		i := i // this i "shadows" the i for the for loop
-		go func() {
-			fmt.Println(i) // body variable i
-		}()
+		// i := i // this i "shadows" the i for the for loop
+		// go func() {
+		// 	fmt.Println(i) // body variable i
+		// }()
+
+		// Fake Fix 3: put the goroutine somewhere else, do not do this
+		// func() {
+		// 	go fmt.Println(i)
+		// }()
+
+		// Fake Fix 4: remove the anonymous function, do not do this
+		// go fmt.Println(i)
 	}
 
 	time.Sleep(10 * time.Millisecond)
@@ -35,7 +43,7 @@ func main() {
 
 func shadowExample() {
 	n := 4
-	{
+	{ // Naked { } Block that only introduces a new lexical scope, rarely used syntax.
 		n := 2 // from here n is the inner n, not the outer
 		fmt.Println("inner: ", n)
 	}
