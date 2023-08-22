e656610631d3c1d5c3b105ce5c4d7ced16887a67
Jim Shaw <jimxshaw@gmail.com>
2023-08-22 18:45:11 -0500

    Update payment example.

--- practical-go/payment/payment.go
+++ practical-go/payment/payment.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	p := Payment{
@@ -9,10 +12,17 @@ func main() {
 		Amount: 360.16,
 	}
 
+	// Payment will only be process once.
+	p.Process()
 	p.Process()
 }
 
 func (p *Payment) Process() {
+	// Idempotent operation.
+	p.once.Do(p.process)
+}
+
+func (p *Payment) process() {
 	fmt.Printf("%s -> $%.2f -> %s\n", p.From, p.Amount, p.To)
 }
 
@@ -20,4 +30,6 @@ type Payment struct {
 	From   string
 	To     string
 	Amount float64 // USD
+
+	once sync.Once // Not exported!
 }
