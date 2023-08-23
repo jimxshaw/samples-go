12764bd8637fa7971c20db96b38466b41eb5478b
Jim Shaw <jimxshaw@gmail.com>
2023-08-22 19:20:50 -0500

    Update counter with two solutions.

--- practical-go/counter/counter.go
+++ practical-go/counter/counter.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 func main() {
+	/* Solution 1: use mutex
+	var mu sync.Mutex
 	count := 0
+	*/
+
+	// count := int64(0)
+	var count int64
 
 	const n = 10
 
@@ -16,9 +23,15 @@ func main() {
 
 	for i := 0; i < n; i++ {
 		go func() {
-			wg.Done()
+			defer wg.Done()
 			for j := 0; j < 10_000; j++ {
-				count++
+				// Solution 1: use mutex
+				// mu.Lock()
+				// count++
+				// mu.Unlock()
+
+				// Solution 2: use atomic
+				atomic.AddInt64(&count, 1)
 			}
 		}()
 	}
