c4a6506405382a66bd75cc3be61d01aafa03d8ae
Jim Shaw <jimxshaw@gmail.com>
2023-09-11 14:53:17 -0500

    Add table testing example.

--- practical-go/nlp/nlp_test.go
+++ practical-go/nlp/nlp_test.go
@@ -6,6 +6,24 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var tokenizeCases = []struct { // anonymous struct
+	text   string
+	tokens []string
+}{
+	{"Hello there!", []string{"hello", "there"}},
+	{"", nil},
+}
+
+// This is called regression testing or table testing.
+func TestTokenizeTable(t *testing.T) {
+	for _, tc := range tokenizeCases {
+		t.Run(tc.text, func(t *testing.T) {
+			tokens := Tokenize(tc.text)
+			require.Equal(t, tc.tokens, tokens)
+		})
+	}
+}
+
 func TestTokenize(t *testing.T) {
 	text := "Hello there!"
 	expected := []string{"hello", "there"}
