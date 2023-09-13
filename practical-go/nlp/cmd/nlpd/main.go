3e5f2ffe5e3dc61aaf08abbaf3a505ac60c13811
Jim Shaw <jimxshaw@gmail.com>
2023-09-13 11:35:01 -0500

    Update tokenizeHandler.

--- practical-go/nlp/cmd/nlpd/main.go
+++ practical-go/nlp/cmd/nlpd/main.go
@@ -16,6 +16,8 @@ func main() {
 	// /health/ is a prefix match.
 	http.HandleFunc("/health", healthHandler)
 
+	http.HandleFunc("/tokenize", tokenizeHandler)
+
 	// Run server.
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("error: %s", err)
@@ -29,24 +31,33 @@ type response struct {
 // tokenizeHandler will read the text from the request body
 // and return JSON in the format: "{"tokens": ["hello", "there"]}".
 func tokenizeHandler(w http.ResponseWriter, r *http.Request) {
-	bodyBytes, err := io.ReadAll(r.Body)
-	if err != nil {
-		log.Fatalf("error: %s", err)
+	if r.Method != http.MethodPost {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
 	}
 
+	// Get, convert and validate the data.
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "can't read", http.StatusBadRequest)
+		return
+	}
+
+	// Do some work.
 	tokens := nlp.Tokenize(string(bodyBytes))
 
+	// Encode and then emit response.
 	respTokens := response{
 		Tokens: tokens,
 	}
 
 	jsonData, err := json.Marshal(respTokens)
 	if err != nil {
-		log.Fatalf("error: %s", err)
+		http.Error(w, "can't encode", http.StatusInternalServerError)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-
 	w.Write(jsonData)
 }
 
